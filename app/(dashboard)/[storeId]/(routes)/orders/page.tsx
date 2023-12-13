18fc93f8556ef5993f71a996e741c64eefe22c60
Vitor Sousa Mesquita <vitorsmfff@yahoo.com>
2023-12-13 12:44:44 -0300

    fix: quantity per item display

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -43,7 +43,7 @@ const OrdersPage = async ({
         quantity: item.orderItems.reduce((total, orderItem) => {
             return total + orderItem.orderQuantity
         }, 0).toString(),
-        quantityPerItem: item.orderItems.map((orderItem) => `--Nome: ${orderItem.product.name}\nQuantidade: ${orderItem.orderQuantity}--`).join('\n\n')
+        quantityPerItem: item.orderItems.map((orderItem) => `--Nome: ${orderItem.product.name} | Quantidade: ${orderItem.orderQuantity}--`).join('\n\n')
     }))
 
     return (
